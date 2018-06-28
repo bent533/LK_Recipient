5d2de3169ce27d39af9d5539c5288ca3c3ca5a04
bent533 <bent533@gmail.com>
2018-06-28 11:57:57 +0300

    new selenium

--- lk_recipint_web_tests/src/test/java/ru/stqa/pft/recipient/LinkTest.java
+++ lk_recipint_web_tests/src/test/java/ru/stqa/pft/recipient/LinkTest.java
@@ -1,17 +1,19 @@
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+//import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
+//import java.util.Date;
 import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
+//import org.openqa.selenium.support.ui.Select;
+//import org.openqa.selenium.interactions.Actions;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.*;
+//import static org.openqa.selenium.OutputType.*;
 
 public class LinkTest {
     ChromeDriver wd;
@@ -36,8 +38,12 @@ public class LinkTest {
       //  new Actions(wd).doubleClick(wd.findElement(By.id("snils"))).build().perform();
      //   wd.findElement(By.cssSelector("div.content-box.login-slils-box")).click();
         wd.findElement(By.linkText("СНИЛС")).click();
-        wd.findElement(By.id("snils")).click();
-        wd.findElement(By.id("snils")).sendKeys("00000060004");
+       By snils = By.cssSelector("input#snils");
+    //    wd.findElement(By.id("snils")).click();
+      //  wd.findElement(By.id("snils")).f;
+
+        wd.findElement(snils).sendKeys("00000060004");
+
 //        wd.findElement(By.id("snils")).clear();
         /*
 
@@ -76,18 +82,18 @@ public class LinkTest {
         wd.findElement(By.cssSelector("span.btn__text")).click();
         wd.findElement(By.cssSelector("span.logo__title")).click();
     }
-    
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+//
+//    @AfterMethod
+//    public void tearDown() {
+//        wd.quit();
+//    }
+//
+//    public static boolean isAlertPresent(ChromeDriver wd) {
+//        try {
+//            wd.switchTo().alert();
+//            return true;
+//        } catch (NoAlertPresentException e) {
+//            return false;
+//        }
+//    }
 }
